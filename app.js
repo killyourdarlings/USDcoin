b7dedc03781b439dfb4362e847ba5715d05c4466
killyoudarlings <jlee286@artic.edu>
2020-11-17 21:21:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello");
+ethereum.autoRefreshOnNetworkChang=false;
+ethereum.enable();
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+const contractAddress = "0xF6d70B1FF611f5beee433a59E191086a6ED7a87C";
+const contractABI=[
+    "function reward(uint256 amt) public"
+];
+const contract = new ethers.Contract(contractAddress, contractABI, provider);
+const tokenWithSigner = contract.connect(signer);
+tokenWithSigner.reward(10);
